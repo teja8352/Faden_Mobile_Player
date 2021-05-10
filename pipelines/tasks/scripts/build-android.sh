9951f0900cfc408c78950b2f2a656ad9e914b5cb
arshadalis <15.arshads@gmail.com>
2021-05-10 21:27:14 +0530

    test ionic commands

--- pipelines/tasks/scripts/build-android.sh
+++ pipelines/tasks/scripts/build-android.sh
@@ -4,12 +4,16 @@ set -ex
 
 cd source
 
-ls -l
-
 node -v
 
 java -version
 
 
+npm install
 
-echo "Welcome to the world of Operations."
+
+ionic cordova platform add android
+ionic cordova build android --prod --aot --release
+keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore HelloWorld-release-unsigned.apk alias_name
+zipalign -v 4 HelloWorld-release-unsigned.apk HelloWorld.apk
