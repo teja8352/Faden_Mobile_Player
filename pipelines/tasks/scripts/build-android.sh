cddc92f1223a207b3d5f01f495ccc9947e62f55d
arshadalis <15.arshads@gmail.com>
2021-05-12 00:24:05 +0530

    check

--- pipelines/tasks/scripts/build-android.sh
+++ pipelines/tasks/scripts/build-android.sh
@@ -18,9 +18,9 @@ export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 cd $ANDROID_HOME
-curl --output sdk-tools-linux.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-echo  Y | unzip sdk-tools-linux.zip
-echo  Y | ./tools/bin/sdkmanager --install 'build-tools;29.0.2' 'platform-tools' 'platforms;android-29' 'tools'
+#curl --output sdk-tools-linux.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+#echo  Y | unzip sdk-tools-linux.zip
+#echo  Y | ./tools/bin/sdkmanager --install 'build-tools;29.0.2' 'platform-tools' 'platforms;android-29' 'tools'
 
 cd $APP_HOME
 
