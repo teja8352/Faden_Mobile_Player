5473bd516c4cffbb4cdb039320324a89207eceee
arshadalis <15.arshads@gmail.com>
2021-05-16 19:46:46 +0530

    build android using our image

--- pipelines/tasks/scripts/build-android.sh
+++ pipelines/tasks/scripts/build-android.sh
@@ -1,29 +1,11 @@
 #!/bin/sh
 
 set -ex
-
 cd source
-
-export APP_HOME=$PWD
 
 echo n | npm install
 npm i -g ionic
 npm i -g cordova
-
-apt update
-echo Y | apt install android-sdk
-
-export ANDROID_HOME=/usr/lib/android-sdk/
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-
-cd ..
-curl --output sdk-tools-linux.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-echo  y | unzip sdk-tools-linux.zip
-echo  y | ./tools/bin/sdkmanager --install 'build-tools;29.0.2' 'platform-tools' 'platforms;android-29' 'tools'
-yes | ./tools/bin/sdkmanager --licenses
-
-cd source
 
 ionic cordova platform add android
 echo n | ionic cordova build android --prod --aot --release
