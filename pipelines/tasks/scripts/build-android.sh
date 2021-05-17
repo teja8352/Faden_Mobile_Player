912fdccb1742c5604a5b77515c41a91b5a7b0154
arshadalis <15.arshads@gmail.com>
2021-05-17 18:48:06 +0530

    create zip from apk

--- pipelines/tasks/scripts/build-android.sh
+++ pipelines/tasks/scripts/build-android.sh
@@ -10,3 +10,4 @@ echo n | ionic cordova build android --prod --aot --release
 #keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
 #jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore HelloWorld-release-unsigned.apk alias_name
 #zipalign -v 4 HelloWorld-release-unsigned.apk HelloWorld.apk
+zip -r output/mobile-player-apk.zip platforms/android/app/build/outputs/apk/release/
