c59b98eaa36a18a16d5f84f0436b41afb9a62937
arshadalis <15.arshads@gmail.com>
2021-05-19 21:03:34 +0530

    make tar file name apk

--- pipelines/tasks/scripts/build-android.sh
+++ pipelines/tasks/scripts/build-android.sh
@@ -11,4 +11,4 @@ echo n | ionic cordova build android --prod --aot --release
 #keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
 #jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore HelloWorld-release-unsigned.apk alias_name
 #zipalign -v 4 HelloWorld-release-unsigned.apk HelloWorld.apk
-tar -czvf ../output/file.tar.gz platforms/android/app/build/outputs/apk/release
+tar -czvf ../output/apk.tar.gz platforms/android/app/build/outputs/apk/release
